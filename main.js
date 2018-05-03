29e5f9c7c23c455e47372618d3f8334582efa61e
spereirag <sebastian.pereira@inria.cl>
2018-05-03 16:45:41 -0300

    Implement nice rotation from psmove websocket feed

--- main.js
+++ main.js
@@ -1,42 +1,17 @@
- var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
 let camera = viewer.camera;
 
 
-
-let rotateFromDifference = (difference)=>{
-    difference  = Cesium.Ellipsoid.WGS84.cartesianToCartographic(difference);    
-    camera.rotateRight(-Cesium.Math.toRadians(difference.longitude)*4) 
+let rotateToDirection = (directionVector) => {
+    let minDifference = 30;
+    let rotationSpeed = -0.5;
+    let magnitude = Math.sign(directionVector[0])*Math.floor(Math.abs(directionVector[0]/15));
+    let lonRotation = rotationSpeed*magnitude;
+    if( Math.abs(directionVector[0]) > minDifference)
+        camera.rotateRight(Cesium.Math.toRadians(lonRotation)) 
 }
 
-
-
-let rotateBetweenPoints = (startPixelCoordinates, endPixelCoordinates) => {
-    let startPosition = new Cesium.Cartesian2(startPixelCoordinates[0],startPixelCoordinates[1])
-    let endPosition = new Cesium.Cartesian2(endPixelCoordinates[0],endPixelCoordinates[1])
-
-    var ellipsoid = viewer.scene.globe.ellipsoid;
-    var start = viewer.camera.pickEllipsoid(startPosition, ellipsoid);
-    var end = viewer.camera.pickEllipsoid(endPosition, ellipsoid);
-
-    if(start != undefined && end != undefined){
-    
-        var scratch = new Cesium.Cartesian3();
-        var difference = Cesium.Cartesian3.subtract(start, end, scratch);
-    
-       
-        rotateFromDifference(difference);
-    }
-}
-
-
-var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);  
-handler.setInputAction((a)=>{
-    // rotateBetweenPoints([a.startPosition.x, a.startPosition.y], [a.endPosition.x,a.endPosition.y]);
-    console.log(a.startPosition, a.endPosition)
-    
-}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
-
-var ws = new WebSocket("ws://192.168.4.16:8765/");
+var ws = new WebSocket("ws://192.168.5.9:8765/");
 let startMovingPoint = [];
 let previousPoint = [];
 let currentPoint = [];
@@ -52,17 +27,11 @@ ws.onmessage = function(event){
 
     if(data.event=="START_MOVING"){
         startMovingPoint = [data.x, data.y]
-        previousPoint = [data.x, data.y];
-    }
-    else if(data.event=="MOVE"){
-        previousPoint = currentPoint.slice();
     }
     
     currentPoint = [data.x , data.y];
 
-    normalizedCurrentPoint = normalize(currentPoint, startMovingPoint);
-    normalizedPreviousPoint = normalize(previousPoint, startMovingPoint);
-
-    console.log(normalizedCurrentPoint, normalizedPreviousPoint);
-    rotateBetweenPoints(normalizedCurrentPoint, normalizedPreviousPoint);
+    directionVector = [startMovingPoint[0]-currentPoint[0],startMovingPoint[1]-currentPoint[1]]
+    console.log('Idderoijcetion vetctor', directionVector);
+    rotateToDirection(directionVector);
 };
