78e8ccf43cfd60b133c0ba6d9bfc372a11244b8f
Jose Galaz <jdgalaz@uc.cl>
2018-05-11 12:12:54 -0300

    add TRIGGER PRESS/RELEASE events handlers

--- client/main.js
+++ client/main.js
@@ -268,22 +268,13 @@ window.onmousedown = (e) => {
         handleTrianglePress(circlePoint);
     }
     else if (e.button === 0) {
-
-        events['rotate'].pressed = true;
-        circlePoint = windowToUnitCircleCoordinates([e.clientX, e.clientY]);
-
-        if (events['rotate'].initialPoint.length == 0) {
-            events['rotate'].initialPoint = circlePoint;
-        }
-
+        handleTriggerPress(circlePoint);        
     }
 };
 
 window.onmouseup = (e) => {
     if (e.button == 0) {
-        events['rotate'].pressed = false;
-        events['rotate'].initialPoint = [];
-        circlePoint = [];
+        handleTriggerRelease();
     }
     //alert(events['rotate'].initialPoint);
 
@@ -319,4 +310,16 @@ function handleMove(currentCirclePoint){
 function handleTrianglePress(currentCirclePoint){
     startEarthquakeFromUnitCircleCoordinates(circlePoint);
 
+}
+
+function handleTriggerPress(currentCirclePoint){
+    events['rotate'].pressed = true;
+    if (events['rotate'].initialPoint.length == 0) {
+        events['rotate'].initialPoint = circlePoint;
+    }
+}
+function handleTriggerRelease(currentCirclePoint){
+    events['rotate'].pressed = false;
+    events['rotate'].initialPoint = [];
+    circlePoint = [];
 }
